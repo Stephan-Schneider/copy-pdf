7b67d9264aba1ff992fcbb6e256fc0235f134e47
stephan <stephanschneider34@hotmail.com>
2026-08-07 23:42:31 +0200

    Add background task for temporary file cleanup and update file upload endpoint with PDF response
    - The upload_file endpoint in main.py now returns the processed PDF file (instead of a link to the temporary file).
    - After returning the processed file, it is deleted in a background task.
    - The unit test in test_main.py has been updated accordingly and now checks for HTTP status code, content type and content of the retuned file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from typing import Annotated
-from fastapi import FastAPI, File, Form, HTTPException
+from fastapi import FastAPI, File, Form, HTTPException, BackgroundTasks
 from fastapi.params import Depends
+from fastapi.responses import FileResponse
+
 
 import app.transform.handle_upload as handle_upload
 from app.transform.handle_pdf import PDFHandler, PDFError
@@ -13,6 +16,16 @@ logger = setup_logging()
 app = FastAPI()
 app.include_router(user_router, prefix="/auth", tags=["auth"])
 
+def remove_file(path: str):
+    try:
+        file_path = Path(path)
+        if  file_path.exists():
+            logger.info(f"Removing temporary file: {file_path}")
+            file_path.unlink()
+            logger.info(f"Temporary file removed: {file_path}")
+    except OSError as e:
+        logger.error(f"Failed to remove temporary file: {e}")
+
 @app.get("/")
 def read_root():
     logger.info("Root endpoint called")
@@ -21,6 +34,7 @@ def read_root():
 @app.post("/upload_file")
 def upload_file(
         user: Annotated[User, Depends(get_current_user)],
+        background_tasks: BackgroundTasks,
         file: Annotated[bytes, File()],
         num_pages: Annotated[list[str] | None, Form(description="List of page numbers to extract")] = None,
         language: Annotated[str, Form(description="Language to use for OCR")] = "deu+eng"
@@ -37,7 +51,16 @@ def upload_file(
         logger.info("Creating temporary file for upload")
         tmp_pdf = handle_upload.create_temp_file(file)
         pdf_handler = PDFHandler(tmp_pdf, num_pages, language)
-        return pdf_handler.transform_pdf()
+        output_file = pdf_handler.transform_pdf()
+        logger.info(f"Temporary file created: {output_file}")
+
+        background_tasks.add_task(remove_file, output_file)
+
+        return FileResponse(
+            path=output_file,
+            media_type="application/pdf",
+            filename="processed.pdf"
+        )
     except OSError as e:
         logger.error(f"Failed to create temporary file: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,29 @@
 import unittest
+from datetime import timedelta
 from pathlib import Path
 from fastapi.testclient import TestClient
 from app.main import app
+from app.routers.user_auth import create_access_token
 
 class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.client = TestClient(app)
+
+        access_token = create_access_token(
+            data={"sub": "testuser"},
+            expires_delta=timedelta(minutes=30)
+        )
+        self.client.headers.update(
+            {"Authorization": f"Bearer {access_token}"}
+        )
 
     def test_upload_file(self):
         data = {"num_pages": ["1-2"], "language": "deu+eng"}
         pdf_file = Path("~/Projekte/Konzepte/ocrpdf/multipage.pdf").expanduser()
 
         with pdf_file.open("rb") as f:
-            response = TestClient(app).post("/upload_file", files={"file": f}, data=data)
+            response = self.client.post("/upload_file", files={"file": f}, data=data)
             self.assertEqual(response.status_code, 200)
-            print(response.text)
-            self.assertTrue("output_" in response.text)
+            self.assertEqual(response.headers["content-type"], "application/pdf")
+            self.assertTrue(response.content.startswith(b"%PDF"))
